6b49f939beee9f388c517515342e0deb5f6f0de8
Isarel Getu <israelgetu101@gmail.com>
2023-02-03 00:06:31 -0500

    Richest Customer Wealth

--- codePractice.js
+++ codePractice.js
@@ -275,3 +275,23 @@
 // console.log(goodPair(data));
 
 // Richest Customer Wealth
+// function richestCW(accounts) {
+//   let maxWealth = 0;
+//   for (let i = 0; i < accounts.length; i++) {
+//     let currentWealth = 0;
+//     for (let j = 0; accounts[i]; j++) {
+//       currentWealth += accounts[i][j];
+//     }
+//     if (currentWealth > maxWealth) {
+//       currentWealth = maxWealth;
+//     }
+//   }
+//   return maxWealth;
+// }
+
+// let accounts = [
+//   [1, 2],
+//   [3, 4],
+// ];
+
+// console.log(richestCW(accounts));
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,20 @@
-function goodPair(arr) {
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; arr.length; i++) {
-      if (arr[i] === arr[j]) {
-        count = count + 1;
-      }
-    }
-  }
-  return count;
-}
+function richestCW(accounts) {
+//   let maxWealth = 0;
+//   for (let i = 0; i < accounts.length; i++) {
+//     let currentWealth = 0;
+//     for (let j = 0; accounts[i]; j++) {
+//       currentWealth += accounts[i][j];
+//     }
+//     if (currentWealth > maxWealth) {
+//       currentWealth = maxWealth;
+//     }
+//   }
+//   return maxWealth;
+// }
 
-console.log(goodPair([1, 1, 2, 3, 1, 1, 3]));
+// let accounts = [
+//   [1, 2],
+//   [3, 4],
+// ];
+
+// console.log(richestCW(accounts));
