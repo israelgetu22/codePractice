333881f77911ac8207d79c56f05b6e0659e871e4
Isarel Getu <israelgetu101@gmail.com>
2023-02-02 23:52:55 -0500

    Richest Customer Wealth

--- codePractice.js
+++ codePractice.js
@@ -259,17 +259,19 @@
 
 // Good Pair
 
-function goodPair(arr) {
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; arr.length; i++) {
-      if (arr[i] === arr[j]) {
-        count = count + 1;
-      }
-    }
-  }
-  return count;
-}
+// function goodPair(arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; arr.length; i++) {
+//       if (arr[i] === arr[j]) {
+//         count = count + 1;
+//       }
+//     }
+//   }
+//   return count;
+// }
 
-let data = [1, 1, 2, 3];
-console.log(goodPair(data));
+// let data = [1, 1, 2, 3];
+// console.log(goodPair(data));
+
+// Richest Customer Wealth
